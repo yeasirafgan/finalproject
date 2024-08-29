b7a0bf7396ea9e2f981b7ecd4277411c054f13e2
Sher yeasir afgan <yeasir.one@gmail.com>
2024-08-29 11:30:52 +0100

    mobiles

--- app/timesheet/page.js
+++ app/timesheet/page.js
@@ -29,28 +29,15 @@ const TimesheetPage = async ({ searchParams }) => {
   };
 
   return (
-    // <main className='flex flex-col md:flex-row justify-center items-start bg-white py-5 space-y-5 md:space-y-0'>
-    //   <div className='bg-white shadow-lg rounded-lg p-8 w-full md:w-1/2 lg:w-1/3'>
-    //     <h1 className='text-lg font-bold mb-6 text-slate-700 text-center md:text-left'>
-    //       How many hours you work today
-    //     </h1>
-    //     <TimesheetForm onSubmit={handleSubmit} username={username} />
-    //   </div>
-
-    //   <div className='bg-white lg:ml-2 shadow-lg rounded-lg p-8 w-full md:w-1/2 lg:w-2/3'>
-    //     <UserTimesheetData username={username} />
-    //   </div>
-    // </main>
-    <main className='flex flex-col md:flex-row justify-center items-start bg-white p-5 space-y-5 md:space-y-0'>
+    <main className='flex flex-col md:flex-row justify-center items-start bg-white py-5 space-y-5 md:space-y-0'>
       <div className='bg-white shadow-lg rounded-lg p-8 w-full md:w-1/2 lg:w-1/3'>
-        <h1 className='text-md font-bold mb-6 text-emerald-900 text-center md:text-left'>
+        <h1 className='text-lg font-bold mb-6 text-slate-700 text-center md:text-left'>
           How many hours you work today
         </h1>
         <TimesheetForm onSubmit={handleSubmit} username={username} />
       </div>
 
-      {/* Adjust width for desktop and mobile versions */}
-      <div className='bg-white shadow-lg rounded-lg p-8 w-full md:w-2/3 lg:w-2/3 lg:ml-2'>
+      <div className='bg-white lg:ml-2 shadow-lg rounded-lg p-8 w-full md:w-1/2 lg:w-2/3'>
         <UserTimesheetData username={username} />
       </div>
     </main>
